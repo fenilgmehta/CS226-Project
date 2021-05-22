0db1844a67f580d37b7afaa099f311d508c43b0d
Fenil Mehta <fenilgmehta@gmail.com>
2021-05-22 12:35:07 +0530

    Update the source code in the simulated memory

--- src/memory.vhdl
+++ src/memory.vhdl
@@ -1,60 +1,54 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_arith.all;	 
-use ieee.std_logic_unsigned.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE ieee.std_logic_arith.ALL;
+USE ieee.std_logic_unsigned.ALL;
 
-library std;
-use std.standard.all;
+LIBRARY std;
+USE std.standard.ALL;
 
-
--- since The Memory is asynchronous read, there is no read signal.
+-- No reed signal is required as memory read operation is asynchronous.
 -- this memory is word addressible
 -- word size = 16 bits
+-- Memory is ASYNC read and SYNC write
 
-entity Memory_asyncread_syncwrite is 
-	port (addr,mem_data_in: in std_logic_vector(15 downto 0); clk,mem_wr: in std_logic;
-				mem_data_out: out std_logic_vector(15 downto 0));
-end entity;
+ENTITY SimulatedRAM IS
+    PORT (
+        addr, mem_data_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        clock, mem_write : IN STD_LOGIC;
+        mem_data_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+    );
+END ENTITY;
 
-architecture MemoryArch of Memory_asyncread_syncwrite is 
+ARCHITECTURE SimulatedRAMArch OF SimulatedRAM IS
 
-type regarray is array(31 downto 0) of std_logic_vector(15 downto 0);   -- defining a new type
-signal SimulatedRAM: regarray:=(
-	0 => x"4054",
-	1 => x"6000",
-	2 => x"c042",
-	3 => x"0210",
-	4 => x"c4c3",
-	7 => x"13be",
-	8 => x"2128",
-	9 => x"0a32",
-	10 => x"c982",
-	11 => x"212a",
-	12 => x"3caa",
-	13 => x"5044",
-	14 => x"8202",
-	16 => x"91c0",
-	18 => x"7000",
-	19 => x"f000",
-	20 => x"0014",
-	21 => x"0002",
-	23 => x"0016",
-	24 => x"ffff",
-	26 => x"ffff",
-	27 => x"0012",
-	others => x"0000");
-	
+    TYPE regarray IS ARRAY(127 DOWNTO 0) OF STD_LOGIC_VECTOR(15 DOWNTO 0); -- defining a new type
+    SIGNAL SimulatedRAMStorage : regarray := (
+        0 => x"3100",
+        1 => x"3300",
+        2 => x"0050",
+        3 => x"0052",
+        4 => x"0051",
+        7 => x"2050",
+        8 => x"3210",
+        9 => x"2052",
+        10 => x"2051",
+        11 => x"4058",
+        12 => x"1040",
+        13 => x"3020",
+        14 => x"5040",
+        16 => x"6200",
+        OTHERS => x"0000"
+    );
 
-begin
-mem_data_out <= SimulatedRAM(conv_integer(addr));
-mem_write:
-process (mem_wr,mem_data_in,addr,clk)
-	begin
-	if(mem_wr = '1') then
-		if(rising_edge(clk)) then
-			SimulatedRAM(conv_integer(addr)) <= mem_data_in;
-		end if;
-	end if;
-	end process;
-end MemoryArch;
+BEGIN
+    mem_data_out <= SimulatedRAMStorage(conv_integer(addr));
+    mem_write : PROCESS (mem_wr, mem_data_in, addr, clk)
+    BEGIN
+        IF (mem_wr = '1') THEN
+            IF (rising_edge(clk)) THEN
+                SimulatedRAMStorage(conv_integer(addr)) <= mem_data_in;
+            END IF;
+        END IF;
+    END PROCESS;
+END MemoryArch;
