99fe22e174ce97ad2226da72488e0379e78144b8
Fenil Mehta <fenilgmehta@gmail.com>
2021-05-23 02:50:32 +0530

    Major fix in ALU - now it behaves like a combinational circuit
    - Earlier ALU was working like a sequential circuit which was generating output only on a rising clock pulse.

--- src/generic_alu.vhdl
+++ src/generic_alu.vhdl
@@ -4,6 +4,8 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 -- use ieee.numeric_std.all;
 
+-- REFER: https://www.fpga4student.com/2017/06/vhdl-code-for-arithmetic-logic-unit-alu.html
+
 entity alu is
     generic(n: natural :=16);
     port (
@@ -18,57 +20,39 @@ end alu;
 
 architecture alu_arch of alu is
 
-    component generic_adder is
-        generic (n: natural :=16);
-        port (
-            input_a : in std_logic_vector (n-1 downto 0);
-            input_b : in std_logic_vector (n-1 downto 0);
-            output_sum : out std_logic_vector (n-1 downto 0);
-            carry : out std_logic
-        );
-    end component;
+    signal temp_result_sum :  std_logic_vector(n-1 downto 0) := (others => '0');
+    signal temp_result_nand : std_logic_vector(n-1 downto 0) := (others => '0');
+    signal temp_c : std_logic := '0';
+    signal temp_z : std_logic := '0';
 
-    component generic_nand is
-        generic (n: natural :=16);
-        port (
-            input_a : in std_logic_vector (n-1 downto 0);
-            input_b : in std_logic_vector (n-1 downto 0);
-            output_nand : out std_logic_vector (n-1 downto 0)
-        );
-    end component;
-
-    signal temp_result_sum :  std_logic_vector(n-1 downto 0);
-    signal temp_result_nand : std_logic_vector(n-1 downto 0);
-    signal temp_output_alu : std_logic_vector(n-1 downto 0);
-    signal temp_c : std_logic;
-    signal temp_z : std_logic;
+    signal temp_output_alu : std_logic_vector(n-1 downto 0) := (others => '0');
+    signal temp_for_carry : std_logic_vector(n downto 0) := (others => '0');
 
 begin
 
-    temp_c <= '0';
-    temp_z <= '0';
+    -- temp_c <= '0';
+    -- temp_z <= '0';
  
-    adder : generic_adder port map (input_a => in_a, input_b => in_b, output_sum => temp_result_sum, carry => temp_c);
-    naand : generic_nand port map (input_a => in_a, input_b => in_b, output_nand => temp_result_nand);
-    
-    process (operation, temp_result_sum, temp_result_nand)
+    process (operation, in_a, in_b, temp_result_sum, temp_result_nand)
     begin
+ 
         case operation is
             when '0' =>
-                temp_output_alu <= temp_result_sum;
-                c_out <= temp_c and not operation;   
-                -- operation 0, means addition and 1 means nand
+                temp_output_alu <= in_a + in_b;
+            when '1' =>
+                temp_output_alu <= in_a nand in_b;
             when others =>
-                temp_output_alu <= temp_result_nand;
+                temp_output_alu <= (others => 'Z');  -- Z denotes high impedance
         end case;
     
-        if temp_output_alu =  x"0000" then
-            temp_z <= '1';
+        if (temp_output_alu = 0) then
+            z_out <= '0';
         end if;
-    
-        output_alu <= temp_output_alu;
-        z_out <= temp_z;    
 
     end process;
+
+    temp_for_carry <= ('0' & in_a) + ('0' & in_b);
+    c_out <= temp_for_carry(n);
+    output_alu <= temp_output_alu;
 
 end alu_arch;
